9973e97078cd1d9e3f7d8fcc94d1ae898c6cf16d
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 18:48:13 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 // import { Layout } from 'components/Layout/Layout';
-import { ContactFilter } from './filter/filter';
-import { ContactForm } from './contact/form';
-import { ContactList } from './contact/list';
+// import { ContactFilter } from './filter/filter';
+import { ContactForm } from './contactForm/form';
+import { ContactList } from './contactList/list';
 
 export const App = () => {
   return (
     <>
       <ContactForm />
-      <ContactFilter />
+      {/* <ContactFilter /> */}
       <ContactList />
     </>
   );
