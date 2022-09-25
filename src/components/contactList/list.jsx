29ea1c9db75bfde4f69105fb69a4df86741f1430
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 21:55:39 +0300

    Update list.jsx

--- src/components/contactList/list.jsx
+++ src/components/contactList/list.jsx
@@ -1,18 +1,36 @@
-// import { deleteContact } from '../../redux/actions.js';
+import { deleteContact } from '../../redux/actions.js';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux'; //, useDispatch
-import ContactItem from '../contactItem/item';
+// import { ContactItem } from '../contactItem/item';
 import css from './list.module.css';
-import { getContacts } from '../../redux/selectors';
-
+// import { getContacts } from '../../redux/selectors';
+import { MdClose } from 'react-icons/md';
 export function ContactList() {
-  // const dispatch = useDispatch();
-  const contacts = useSelector(getContacts());
+  const dispatch = useDispatch();
+  const contacts = useSelector(state =>
+    state.contactsReducer.items.filter(contact =>
+      contact.name.toLowerCase().includes(state.contactsReducer.filter)
+    )
+  );
+  const delContacts = data => {
+    dispatch(deleteContact(data));
+  };
 
   return (
     <ul className={css.list}>
       {contacts.map(({ id, name, number }) => (
         <li className={css.item} key={id}>
-          <ContactItem user={name} tel={number} delId={id} />
+          <div className={css.info}>
+            {name}: {number}
+            <button
+              className={css.btn}
+              type="button"
+              onClick={() => delContacts(id)}
+            >
+              <MdClose size={24} />
+            </button>
+          </div>
+          {/* <ContactItem user={name} tel={number} delId={id} /> */}
         </li>
       ))}
     </ul>
