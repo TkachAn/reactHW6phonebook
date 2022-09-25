bd492d32bd9364e127baeed7acbd2326a7f43114
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 20:22:01 +0300

    Update filter.jsx

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,14 +1,22 @@
+import { filterContact } from '../../redux/actions.js';
+import { useSelector, useDispatch } from 'react-redux';
 import css from './filter.module.css';
 
-function Filter({ value, onChange }) {
+export function Filter() {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.contactsReducer.filter);
+
+  const changeFilter = data => {
+    dispatch(filterContact(data));
+  };
   return (
     <label className={css.label}>
       Find contacts by name
       <input
         className={css.input}
         type="text"
-        value={value}
-        onChange={onChange}
+        value={filter}
+        onChange={e => changeFilter(e.target.value)}
       />
     </label>
   );
