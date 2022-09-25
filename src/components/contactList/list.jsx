6f9f03b9908eabb63bfe23010477c9dad98a156c
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 20:22:04 +0300

    Update list.jsx

--- src/components/contactList/list.jsx
+++ src/components/contactList/list.jsx
@@ -1,15 +1,18 @@
+// import { deleteContact } from '../../redux/actions.js';
+import { useSelector } from 'react-redux'; //, useDispatch
 import ContactItem from '../contactItem/item';
 import css from './list.module.css';
-// import {n}
 import { getContacts } from '../../redux/selectors';
-import { useSelector } from 'react-redux';
+
 export function ContactList() {
-  const contacts = useSelector(getContacts);
+  // const dispatch = useDispatch();
+  const contacts = useSelector(getContacts());
+
   return (
     <ul className={css.list}>
-      {contacts.map(contact => (
-        <li className={css.item} key={contact.id}>
-          <ContactItem contact={contact} />
+      {contacts.map(({ id, name, number }) => (
+        <li className={css.item} key={id}>
+          <ContactItem user={name} tel={number} delId={id} />
         </li>
       ))}
     </ul>
