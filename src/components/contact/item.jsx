4fd1375794fe3cbcaeae8242b840d343df318586
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 07:21:22 +0300

    Update item.jsx

--- src/components/contact/item.jsx
+++ src/components/contact/item.jsx
@@ -1,16 +1,16 @@
 import { useDispatch } from 'react-redux';
-// import { MdClose } from 'react-icons/md';
-import { deleteContact } from '../../redux/phoneSlice';
+import { MdClose } from 'react-icons/md';
+import { deleteContact } from '../../redux/contactSlice';
 import css from './item.module.css';
 
-export function ContactItem({ phone }) {
+export function ContactItem({ contact }) {
   const dispatch = useDispatch();
-  const handleDelete = () => dispatch(deleteContact(phone.id));
+  const handleDelete = () => dispatch(deleteContact(contact.id));
   return (
     <div className={css.info}>
-      {phone.user}: {phone.tel}
+      {contact.user}: {contact.tel}
       <button className={css.btn} type="button" onClick={handleDelete}>
-        X
+        <MdClose size={24} />
       </button>
     </div>
   );
