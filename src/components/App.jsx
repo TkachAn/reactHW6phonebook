35fc41babe3e0d988d66e645d4f0d4d2ffa90643
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 21:55:51 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
 // import { Layout } from 'components/Layout/Layout';
+import { useState } from 'react';
 import { Filter } from './filter/filter';
 import { ContactForm } from './contactForm/form';
 import { ContactList } from './contactList/list';
-
+import { loadFromStorage } from './storageService/storageService';
+import css from './App.module.css';
 export const App = () => {
+  const [contacts] = useState(loadFromStorage('phonebook'));
   return (
-    <>
+    <div className={css.container}>
+      <h1>Phonebook</h1>
       <ContactForm />
-      <Filter />
-      <ContactList />
-    </>
+      <h2>Contacts</h2>
+      {contacts.length > 1 && <Filter />}
+      {contacts.length > 0 ? (
+        <ContactList />
+      ) : (
+        <p>Your phonebook is empty. Please add contact.</p>
+      )}
+    </div>
   );
 };
