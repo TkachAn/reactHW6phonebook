968f992fb35db1484f5c3f87ed6caf6a42a37388
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-25 18:47:37 +0300

    Create form.jsx

---
+++ src/components/contactForm/form.jsx
@@ -0,0 +1,71 @@
+import { nanoid } from '@reduxjs/toolkit';
+import { useState } from 'react';
+import css from './form.module.css';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/contactSlice/contactSlice';
+
+export function ContactForm() {
+  //onSubmit
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
+
+  const handleChange = e => {
+    const { name, value } = e.currentTarget;
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
+  };
+  /** */
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const data = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    formSubmit(data);
+    form.reset();
+  };
+  /** */
+
+  const formSubmit = data => {
+    dispatch(addContact(data));
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        Name
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          placeholder="enter your name, please."
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          placeholder="enter your phone number, please."
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
