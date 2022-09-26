4ac75e1f29ca2dc0cb27b1d82969a23d4ac58ebf
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-26 15:17:11 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,3 @@
-// import { contactsReducer } from './reducer';
 export const getContacts = state =>
   state.contactsReducer.items.filter(contact =>
     contact.name.toLowerCase().includes(state.contactsReducer.filter)
