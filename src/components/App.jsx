9d6a2305fc7b36b235f78dd2c9d71311c4836553
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-26 13:36:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 // import { Layout } from 'components/Layout/Layout';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Filter } from './filter/filter';
 import { ContactForm } from './contactForm/form';
 import { ContactList } from './contactList/list';
-import { loadFromStorage } from './storageService/storageService';
+// import { useDispatch } from 'react-redux'; loadFromStorage,
+import { contacts } from './storageService/storageService';
+
 import css from './App.module.css';
 export const App = () => {
-  const [contacts] = useState(loadFromStorage('phonebook'));
+  // const dispatch = useDispatch();
+  // const contacts = loadFromStorage('phonebook');
+  // console.log('contacts.length', contacts); //.length
+  // console.log('ContactList', ContactList);
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
