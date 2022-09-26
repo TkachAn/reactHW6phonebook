5fc901194c1da0ce8fb4ebdd1fa5a89f5b4b9554
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-26 15:05:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,19 @@
-// import { Layout } from 'components/Layout/Layout';
-import { useState, useEffect } from 'react';
 import { Filter } from './filter/filter';
 import { ContactForm } from './contactForm/form';
 import { ContactList } from './contactList/list';
-// import { useDispatch } from 'react-redux'; loadFromStorage,
-import { contacts } from './storageService/storageService';
+import { useSelector } from 'react-redux';
 
 import css from './App.module.css';
 export const App = () => {
-  // const dispatch = useDispatch();
-  // const contacts = loadFromStorage('phonebook');
-  // console.log('contacts.length', contacts); //.length
-  // console.log('ContactList', ContactList);
+  const contacts = useSelector(state => state.contactsReducer);
+
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm />
       <h2>Contacts</h2>
-      {contacts.length > 1 && <Filter />}
-      {contacts.length > 0 ? (
+      {contacts.items.length > 1 && <Filter />}
+      {contacts.items.length > 0 ? (
         <ContactList />
       ) : (
         <p>Your phonebook is empty. Please add contact.</p>
