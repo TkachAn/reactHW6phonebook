e7955aa15580f395eea01fa3e9ab4f020e954ce6
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-28 10:00:58 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,4 @@
 export const getContacts = state => state.contactsReducer.items;
+// export const getContacts = state => state.contactsReducer.items;
 //.filter(contact =>contact.name.toLowerCase().includes(state.contactsReducer.filter));
+//export const getContacts = state => state.contacts.items;
